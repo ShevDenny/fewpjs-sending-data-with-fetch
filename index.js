273ebc140c4f45e12fa9e6da8acd63bb011a331b
ShevDenny <sdenny904@gmail.com>
2021-05-24 15:03:26 -0400

    commit tree 77be82821813fda16d5dcc4f16b4b6b14fe2e066

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(response => response.json()) 
+    .then(object => {
+        document.body.textContent = object.id
+    })
+    .catch(error => {
+        alert("Uh oh, something went wrong!");
+        document.body.textContent = error.message
+    })
+
+}
